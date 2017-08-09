d10669ad548bf6dac40fe96606edfdec6cef5cf1
Ethan Guo <ethan.guo@teledyne.com>
2017-08-09 13:39:18 -0400

    Restructured detector.py  - Added SensorDetectorResult class

--- detector.py
+++ detector.py
@@ -5,11 +5,72 @@ from sklearn.cluster import MeanShift, estimate_bandwidth
 from preprocess import *
 from hsv import *
 
+class SensorDetectorResult:
+	def __init__(self, sensorDetector, matches, match_map):
+		self.matches = matches
+		self.match_map = match_map
+
+		self.scores = match_map[tuple(np.transpose(matches))]
+		self.rss = np.sum(1 - self.scores)
+		self.rsquared = self.rss / len(self)
+
+		self.pattern_shape = sensorDetector.pattern.shape[:2]		
+		self._color_gradient = lambda val: (0, val*2*255, 255) if val < 0.5 else (0, 255, (1-val)*2*255) #BGR
+
+	def __len__(self):
+		return len(self.matches)
+
+	def __getitem__(self, key):
+		return self.matches[key]
+
+	def __iter__(self):
+		return iter(self.matches)
+
+	def paint(self, image):
+		draw_image = np.copy(image)
+
+		pattern_h, pattern_w = self.pattern_shape
+
+		for match, score in zip(self.matches, self.scores):
+			y, x = match
+
+			color = self._color_gradient(score)
+
+			cv2.rectangle(draw_image, 
+				(x, y), (x + pattern_w, y + pattern_h),
+				color=color, thickness=1)
+
+			cv2.circle(draw_image,
+				(x + pattern_w//2, y + pattern_h//2), radius=2,
+				color=color, thickness=-1)
+
+		text1 = "Number of sensors:%3d" %(len(self.matches))
+		text2 = "Average r-squared:%9.6f" %(self.rsquared)
+		text3 = "RSS:%9.6f" %(self.rss)
+		cv2.putText(draw_image, text1, (10, 25), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0,0,255))
+		cv2.putText(draw_image, text2, (10, 50), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0,0,255))
+		cv2.putText(draw_image, text3, (10, 75), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0,0,255))
+
+		return draw_image 	
+
+	# def score(self, y, x):
+	# 	return self.match_map[y, x]
+
+	# def _color_gradient(self, val):
+	# 	if val < 0.5:
+	# 		r = 255
+	# 		g = val * 2 * 255
+	# 	else:
+	# 		r = (1 - val) * 2 * 255
+	# 		g = 255
+	# 	return (0, g, r)
+
+
 class SensorDetector:
 	def __init__(self, pattern, params=None, **kwargs):
+		# Available parameters
+		self.preprocess = lambda x: x
 		self.pattern = pattern
-		self.threshold_block_radius = 5
-		self.threshold_c = 7
 		self.match_method = cv2.TM_CCOEFF_NORMED
 		self.match_threshold = 0.8
 		self.clustering_bandwidth = 40
@@ -24,72 +85,41 @@ class SensorDetector:
 			else:
 				raise AttributeError("Unknown parameter: " + k)
 
+		# Preprocess pattern
+		self.pattern_proc = self.preprocess(self.pattern)
+
 		self.clusterer = MeanShift(self.clustering_bandwidth)
 
-	def preprocess(self, image):
-		output = image #skip
+	# def preprocess(self, image):
+	# 	output = image #skip
 
-		# output = canny(image, 32)
-		# output = sat_mask(image, self.threshold_block_radius, self.threshold_c)
-		# output = get_sat(image)
+	# 	# output = canny(image, 32)
+	# 	# output = sat_mask(image, self.threshold_block_radius, self.threshold_c)
+	# 	# output = get_sat(image)
 
-		return output
+	# 	return output
 
 	def detect(self, image):
-		pattern_proc = self.preprocess(self.pattern)
 		image_proc = self.preprocess(image)
 
-		match_map = cv2.matchTemplate(image_proc, pattern_proc, self.match_method)
+		match_map = cv2.matchTemplate(image_proc, self.pattern_proc, self.match_method)
 
-		matches = np.transpose(np.where(match_map >= self.match_threshold))
+		candidate_matches = np.transpose(np.where(match_map >= self.match_threshold))
 
-		if 0 in matches.shape:
+		if 0 in candidate_matches.shape:
 			raise Warning("0 matches detected")
 			return np.array([]), match_map
 
-		# print(self.clustering_bandwidth)
-
 		self.clusterer.set_params(bandwidth=self.clustering_bandwidth)
-		self.clusterer.fit(matches)
+		self.clusterer.fit(candidate_matches)
 		labels = self.clusterer.labels_
 		centers = self.clusterer.cluster_centers_
 
 		# print(len(np.unique(labels)))
 
-		points = np.int_(centers)
+		matches = np.int_(centers)
 
 		# cv2.imshow("pattern_proc", pattern_proc)
 		# cv2.imshow("image_proc", image_proc)
 
-		return points, match_map
-
-	def draw(self, image):
-		matches, match_map = self.detect(image)
-		draw_image = np.copy(image)
-
-		pattern_h, pattern_w = self.pattern.shape[:2]
-
-		color_gradient = lambda val: (0, val*510, 255) if val < 0.5 else (0, 255, (1-val)*510)
-
-		for pt_y, pt_x in matches:
-			color = color_gradient(match_map[pt_y, pt_x])
-			cv2.rectangle(draw_image, 
-				(pt_x, pt_y), (pt_x + pattern_w, pt_y + pattern_h),
-				color, 1)
-
-			cv2.circle(draw_image,
-				(pt_x + pattern_w//2, pt_y + pattern_h//2),
-				2, color, -1)
-
-			# cv2.putText()
-
-		return draw_image
-
-	# def color_gradient(self, val):
-	# 	if val < 0.5:
-	# 		r = 255
-	# 		g = val * 2 * 255
-	# 	else:
-	# 		r = (1 - val) * 2 * 255
-	# 		g = 255
-	# 	return (0, g, r)
+		return SensorDetectorResult(self, matches, match_map)
--- main.py
+++ main.py
@@ -30,34 +30,24 @@ def main():
 	onChange = lambda x: None # Do nothing function
 
 	cv2.namedWindow(WINDOW_NAME)
-	cv2.createTrackbar("block_radius", WINDOW_NAME, 5, 50, onChange)
-	cv2.createTrackbar("c", WINDOW_NAME, 7, 50, onChange)
 	cv2.createTrackbar("match_threshold_percent", WINDOW_NAME, 70, 100, onChange)
 	cv2.createTrackbar("clustering_bandwidth", WINDOW_NAME, 40, 100, onChange) #permille
 	# cv2.createTrackbar("match_method", WINDOW_NAME, 5, 5, onChange)
 
 	while True:
+		detector.match_threshold = cv2.getTrackbarPos("match_threshold_percent", WINDOW_NAME) / 100
+		detector.clustering_bandwidth = cv2.getTrackbarPos("clustering_bandwidth", WINDOW_NAME) or 1
+
+		# Stopwatch execution of detector.detect
 		start_time = time()
 
-		detector.threshold_block_radius = cv2.getTrackbarPos("block_radius", WINDOW_NAME) or 1
-		detector.threshold_c = cv2.getTrackbarPos("c", WINDOW_NAME)
-		detector.match_threshold = cv2.getTrackbarPos("match_threshold_percent", WINDOW_NAME) / 100
-		detector.clustering_bandwidth = cv2.getTrackbarPos("clustering_bandwidth", WINDOW_NAME)
-
-		matches, match_map = detector.detect(image)
-
+		matches = detector.detect(image)
+		
 		time_elapsed = time() - start_time
 		print(time_elapsed)
 		
-		# print(np.amin(match_map), np.amax(match_map))
-
 		# Display results
-		result = detector.draw(image)
-
-		# text = "Time elapsed:%9.6f   Number of sensors:%3d" %(time_elapsed, len(matches))
-		text = "Number of sensors:%3d" %(len(matches))
-
-		cv2.putText(result, text, (10, 25), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0,0,255))
+		result = matches.paint(image)
 
 		cv2.imshow(WINDOW_NAME, result)
 		
