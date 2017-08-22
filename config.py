8d67a3f82c05ff9dfef18398789f2f4b7b7cd556
Ethan Guo <ethan.guo@teledyne.com>
2017-08-22 17:35:48 -0400

    Doc'ed config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
+"""This module deals with loading and using YAML configuration files."""
 from yaml import safe_load
 
 def YAMLObject(obj):
+	"""Constructs a YAMLDict or a YAMLList if the object is of the corresponding type, otherwise returns the object as-is."""
 	if type(obj) is dict:
 		return YAMLDict(obj)
 	elif type(obj) is list:
@@ -9,6 +11,7 @@ def YAMLObject(obj):
 		return obj
 
 class YAMLDict (dict):
+	"""A subclass of dict whose values can also be accessed as attributes. That is, `a["b"]["c"]` can be instead written as `a.b.c`."""
 	def __init__(self, obj):
 		obj = {key: YAMLObject(value) for key, value in obj.items()}
 		dict.__init__(self, obj)
@@ -17,11 +20,20 @@ class YAMLDict (dict):
 		return dict.__getitem__(self, name)
 
 class YAMLList (list):
+	"""A subclass of list which converts dict values to YAMLDict values."""
 	def __init__(self, obj):
 		obj = [YAMLObject(item) for item in obj]
 		list.__init__(self, obj)
 
 def loadYAML(path):
+	"""Loads a YAML file at the specified path, and returns it as a YAMLObject.
+	
+	Args:
+	    path (str): Path to the YAML file.
+	
+	Returns:
+	    YAMLDict or YAMLList: The root item in the given file, as a YAMLObject.
+	"""
 	f = open(path)
 	obj = safe_load(f)
 	return YAMLObject(obj)
